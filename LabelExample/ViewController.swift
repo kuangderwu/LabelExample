ed1fba278b549a4644c0a6ab4496afabb73f120f
Kuang-Der Wu <apple@applede-macbook-air.local>
2018-10-21 15:57:59 +0800

    Initial Commit

---
+++ LabelExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LabelExample
+//
+//  Created by apple on 2018/10/21.
+//  Copyright © 2018 Kuang-Der Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
